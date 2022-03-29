97f02191bf1c90d90f9da60de9922064ec08581b
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2022-03-29 14:44:22 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,24 @@
+btn1 = document.getElementById("submit-btn");
+btn1.setAttribute('onclick', 'pushButton1(this.id)');
+
+function pushButton1(clicked_id) {
+    let name = document.getElementById("user-name");
+    let displayname = name.innerHTML;
+    let user_id = document.getElementById("user_id");
+    let userid = user_id.innerHTML;
+    // let key = document.getElementById("keyword");
+    // let key = key.value;
+
+    let resurl=(`https://share.streamlit.io/nyumallc/rensen/main/Buy_streamlitpy.py??&userid=${userid}&displayname=${displayname}`)
+    location.href=resurl
+    
+    // liff.sendMessages([{
+    //     'type': 'text',
+    //     'text': p_title + "の注文をリクエストしました。"
+    //   }]).then(function() {
+    //     // document.getElementById('log').value += 'sendMessagesText completed\n';
+    //   }).catch(function(error) {
+    //     // document.getElementById('log').value += 'sendMessagesText()=' + error + '\n';
+    //   });
+      
+    };
