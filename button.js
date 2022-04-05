48fe57b9671d80a7a991dc07ab216db1642da3ca
Nyumallc <95501317+Nyumallc@users.noreply.github.com>
2022-04-05 22:32:36 +0900

    main

--- button.js
+++ button.js
@@ -1,4 +1,4 @@
-document.addEventListener('deviceready', function(){
+document.addEventListener('load', function(){
 btn1 = document.getElementById("but1");
 btn2 = document.getElementById("but2");
 btn3 = document.getElementById("but3");
